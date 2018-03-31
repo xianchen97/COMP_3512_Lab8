120a1d3bcd2c80f6d9881307b50f816fab96dd85
Xian Chen <xianchen97@gmail.com>
2018-03-30 22:04:50 -0700

    final push

--- Lab 8/UnitTest1/UnitTest1/unittest1.cpp
+++ Lab 8/UnitTest1/UnitTest1/unittest1.cpp
@@ -10,6 +10,7 @@ namespace UnitTest1
 	TEST_CLASS(UnitTest1)
 	{
 		FixedList<int, 5> v;
+		FixedList<char, 3> q;
 	public:
 		
 		TEST_METHOD(TestMethod1)
@@ -30,6 +31,38 @@ namespace UnitTest1
 		TEST_METHOD(TestMethod3)
 		{
 			Assert::AreEqual(1, v.get(2));
+		}
+
+		TEST_METHOD(TestMethod4)
+		{
+			Assert::AreEqual(1, v.getFirstIndex(1));
+		}
+		TEST_METHOD(TestMethod5)
+		{
+			Assert::AreEqual(4, v.get(2));
+		}
+		TEST_METHOD(TestMethod6)
+		{
+			q.add('a');
+			q.add('r');
+			q.add('x');
+			Assert::AreEqual('x', q.remove('x'));
+		}
+		TEST_METHOD(TestMethod7)
+		{
+			Assert::AreEqual('r', q.remove('r'));
+		}
+		TEST_METHOD(TestMethod8)
+		{
+			Assert::AreEqual(1, v[1]);
+		}
+		TEST_METHOD(TestMethod9)
+		{
+			Assert::AreEqual(1, v[2]);
+		}
+		TEST_METHOD(TestMethod10)
+		{
+			Assert::AreEqual(2, v[4]);
 		}
 	};
 }
